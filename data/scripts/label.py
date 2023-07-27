d9174df0a31fdc78ec19e3c2d306ef801abc949c
Evan Biggins <memo4eb@gmail.com>
2023-07-27 15:33:11 -0500

    Implemented stretch goals (includes streak, timer, and reset).

---
+++ data/scripts/label.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+class Label():
+    def __init__(self, screen, txt, location, size=(150, 50), bgc=BLACK, fgc=WHITE, font_name="comicsans", font_size=40):
+        self.txt = txt
+        self.size = size
+        self.bgc = bgc
+        self.fgc = fgc
+        self.screen = screen
+        self.x, self.y = location
+        
+        self.surface = pygame.surface.Surface(size)
+        self.rect = self.surface.get_rect(topleft=(self.x, self.y))
+        
+        self.font = pygame.font.SysFont(font_name, font_size)
+        self.txt_surf = self.font.render(self.txt, 1, fgc)
+        self.txt_rect = self.txt_surf.get_rect(topleft=self.rect.topleft)
+
+    def draw(self):
+        self.surface.fill(self.bgc)
+        self.surface.blit(self.surface, self.rect)
+        self.screen.blit(self.txt_surf, self.txt_rect)
+        pygame.display.update()
+
+    def update(self):
+        self.txt_surf = self.font.render(self.txt, 1, self.fgc)
+        self.surface = pygame.surface.Surface(self.size)
+        self.rect = self.surface.get_rect(topleft=(self.x, self.y))
+        self.txt_rect = self.txt_surf.get_rect(topleft=self.rect.topleft)
+        self.draw()
+
+    def set_default_size(self):
+        self.size = (self.txt_surf.get_width() + 10, self.txt_surf.get_height() + 10)
+
+    def get_width(self):
+        return self.size[0]
+    
+    def get_height(self):
+        return self.size[1]
+
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import math
 import pygame
 import random
 import time
 import os
 from data.scripts.button import Button # By importing Button we can access methods from the Button class
+from data.scripts.label import Label
 
 pygame.init()
+pygame.font.init()
+pygame.mixer.init()
 
 clock = pygame.time.Clock()
 
 # Constants
 SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+SCREEN_HEIGHT = 550
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -22,6 +26,14 @@ BLUE_ON = (0, 0, 255)
 BLUE_OFF = (0, 0, 227)
 YELLOW_ON = (255, 255, 0)
 YELLOW_OFF = (227, 227, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# HUD Constants
+HUD_FONT = pygame.font.SysFont("comicsans", 40)
+HUD_HEIGHT = 40
+HUD_WIDTH = SCREEN.get_width()
+HUD_BG = pygame.Rect(0, 0, HUD_WIDTH, HUD_HEIGHT)
 
 # Pass in respective sounds for each color
 GREEN_SOUND = pygame.mixer.Sound(os.path.join("data", "assets", "bell1.mp3")) # bell1
@@ -30,21 +42,54 @@ BLUE_SOUND = pygame.mixer.Sound(os.path.join("data", "assets", "bell3.mp3")) # b
 YELLOW_SOUND = pygame.mixer.Sound(os.path.join("data", "assets", "bell4.mp3")) # bell4
 
 # Button Sprite Objects (REPLACE NONE VALUES!)
-green = Button(GREEN_ON, GREEN_OFF, GREEN_SOUND, 10, 10)
-red = Button(RED_ON, RED_OFF, RED_SOUND, 260, 10)
-blue = Button(BLUE_ON, BLUE_OFF, BLUE_SOUND, 260, 260)
-yellow = Button(YELLOW_ON, YELLOW_OFF, YELLOW_SOUND, 10, 260)
+green = Button(GREEN_ON, GREEN_OFF, GREEN_SOUND, 10, HUD_HEIGHT)
+red = Button(RED_ON, RED_OFF, RED_SOUND, 260, HUD_HEIGHT)
+blue = Button(BLUE_ON, BLUE_OFF, BLUE_SOUND, 260, 260 + HUD_HEIGHT)
+yellow = Button(YELLOW_ON, YELLOW_OFF, YELLOW_SOUND, 10, 260 + HUD_HEIGHT)
 
 # Variables
 colors = ["green", "red", "blue", "yellow"]
 cpu_sequence = []
 choice = ""
+streak = 0
+reset_rect = None
 
-'''
-Draws game board
-'''
+
+# Generates the HUD
+def generate_hud(show_reset=False, show_time=False, time_left=""):
+    global clock, reset_rect
+    clock.tick(60)
+
+    pygame.draw.rect(SCREEN, BLACK, HUD_BG)
+
+    streak_text = Label(SCREEN, "Streak: " + str(streak), (5, 5))
+    streak_text.set_default_size()
+
+    reset_text = Label(SCREEN, "reset", (250, 5), fgc=BLACK)
+    reset_text.set_default_size()
+    reset_text.x = (SCREEN.get_rect().centerx) - (reset_text.get_width()/2)
+
+    timer_text = Label(SCREEN, "Time: ", (400, 5))
+    timer_text.set_default_size()
+    timer_text.rect.x = SCREEN.get_width() - timer_text.get_width()
+    
+    #flags
+    if show_reset: 
+        reset_text.fgc = WHITE
+
+    if show_time:
+        timer_text.txt = "Time: " + time_left
+    
+    streak_text.update()
+    reset_text.update()
+    timer_text.update()
+    reset_rect = reset_text.rect
+
 def draw_board():
     # Call the draw method on all four button objects
+    #if(time): print("drawing: " + str(time_left))
+    SCREEN.fill(BLACK)
+    generate_hud()
     green.draw(SCREEN)
     red.draw(SCREEN)
     blue.draw(SCREEN)
@@ -92,10 +137,21 @@ If player is unable to select a color within 3 seconds then the game is
 over and the pygame window closes.
 '''
 def player_turn():
+    global streak, clock
     turn_time = time.time()
     players_sequence = []
+    handled = None
+    count = 0
+
     while time.time() <= turn_time + 3 and len(players_sequence) < len(cpu_sequence):
+        handled = False
+        show_time = True
+        
+        # Calculate remaining time in turn
+        time_left = str(math.ceil((turn_time + 3) - time.time()))
+
         for event in pygame.event.get():
+            if event.type == pygame.QUIT: end_session()
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             # button click occured
             # Grab the current position of mouse here
@@ -105,24 +161,39 @@ def player_turn():
                     players_sequence.append("green") # add to player sequence
                     check_sequence(players_sequence) # check if player choice was correct
                     turn_time = time.time() # reset timer
+                    handled = True
                 # Check other three options
                 elif red.selected(pos):
                     red.update(SCREEN)
                     players_sequence.append("red")
                     check_sequence(players_sequence)
                     turn_time = time.time()
+                    handled = True
                 elif blue.selected(pos):
                     blue.update(SCREEN)
                     players_sequence.append("blue")
                     check_sequence(players_sequence)
                     turn_time = time.time()
+                    handled = True
                 elif yellow.selected(pos):
                     yellow.update(SCREEN)
                     players_sequence.append("yellow")
                     check_sequence(players_sequence)
                     turn_time = time.time()
+                    handled = True
+                if handled:
+                    break
+        
+        # Generate updated HUD every 300000 iterations to reduce timer flickering (perhaps refactoring or multithreading could fix this issue)
+        if count % 300000 == 0: generate_hud(show_time=show_time, time_left=time_left)
+        count += 1
+    
+    streak += 1
+    show_time = False
+    time_left = ""
+    count = 0
 
-    # If player does not select a button within 3 seconds then the gamecloses
+    # If player does not select a button within 3 seconds then game_over state initiates
     if not time.time() <= turn_time + 3:
         game_over()
 
@@ -136,17 +207,37 @@ def check_sequence(players_sequence):
 '''
 Quits game and closes pygame window
 '''
-def game_over():
+def end_session():
+    pygame.display.quit()
     pygame.quit()
     quit()
+
+# Allows player option to reset within "stall_time" seconds, otherwise ends the session
+def game_over():
+    global reset_rect, cpu_sequence, choice, streak
+    
+    generate_hud(show_reset=True)
+    
+    start = time.time()
+    stall_time = 5
+    broken = False
+    
+    while time.time() <= start + stall_time and not broken:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and reset_rect.collidepoint(pygame.mouse.get_pos()):
+                cpu_sequence = []
+                choice = ""
+                streak = 0
+                broken = True
+                break
+    if not broken: end_session()
 
 # Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.display.quit()
-            pygame.quit()
-            quit()
+            end_session()
+    
     pygame.display.update()
     
     draw_board() # draws buttons onto pygame screen
